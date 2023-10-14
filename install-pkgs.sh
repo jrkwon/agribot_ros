f93bac5ef74529d8c845ced632e55ea0649a6e38
Jaerock <jrkwon@umich.edu>
2023-10-14 10:48:27 -0400

    add pkgs for slam

--- install-pkgs.sh
+++ install-pkgs.sh
@@ -1,28 +1,29 @@
-sudo apt install build-essential
-sudo apt install libasio-dev
-sudo apt install ros-melodic-teleop-twist-keyboard
-sudo apt install ros-melodic-ros-control
-sudo apt install ros-melodic-ros-controllers
-sudo apt install ros-melodic-joint-state-publisher-gui 
-sudo apt install ros-melodic-robot-state-publisher 
-sudo apt install ros-melodic-roslint
-sudo apt install ros-melodic-teleop-twist-joy
-sudo apt install ros-melodic-move-base
-sudo apt install ros-melodic-rqt 
-sudo apt install ros-melodic-rqt-common-plugins
-sudo apt install ros-melodic-rqt-robot-plugins 
-sudo apt install ros-melodic-rqt-robot-steering
-sudo apt install ros-melodic-dwa-local-planner
-sudo apt install ros-melodic-lms1xx
-sudo apt install ros-melodic-velodyne-description
-sudo apt install ros-melodic-realsense2-camera
-sudo apt install ros-melodic-realsense2-description
-sudo apt install ros-melodic-robot-localization
-sudo apt install ros-melodic-interactive-marker-twist-server
-sudo apt install ros-melodic-twist-mux
-sudo apt install ros-melodic-joy
-sudo apt install ros-melodic-teleop-twist-joy
-sudo apt install ros-melodic-pointcloud-to-laserscan
-sudo apt install ros-melodic-imu-filter-madgwick
-sudo apt install ros-melodic-rviz-imu-plugin
-
+sudo apt install -y build-essential
+sudo apt install -y libasio-dev
+sudo apt install -y ros-melodic-teleop-twist-keyboard
+sudo apt install -y ros-melodic-ros-control
+sudo apt install -y ros-melodic-ros-controllers
+sudo apt install -y ros-melodic-joint-state-publisher-gui 
+sudo apt install -y ros-melodic-robot-state-publisher 
+sudo apt install -y ros-melodic-roslint
+sudo apt install -y ros-melodic-teleop-twist-joy
+sudo apt install -y ros-melodic-move-base
+sudo apt install -y ros-melodic-rqt 
+sudo apt install -y ros-melodic-rqt-common-plugins
+sudo apt install -y ros-melodic-rqt-robot-plugins 
+sudo apt install -y ros-melodic-rqt-robot-steering
+sudo apt install -y ros-melodic-dwa-local-planner
+sudo apt install -y ros-melodic-lms1xx
+sudo apt install -y ros-melodic-velodyne-description
+sudo apt install -y ros-melodic-realsense2-camera
+sudo apt install -y ros-melodic-realsense2-description
+sudo apt install -y ros-melodic-robot-localization
+sudo apt install -y ros-melodic-interactive-marker-twist-server
+sudo apt install -y ros-melodic-twist-mux
+sudo apt install -y ros-melodic-joy
+sudo apt install -y ros-melodic-teleop-twist-joy
+sudo apt install -y ros-melodic-pointcloud-to-laserscan
+sudo apt install -y ros-melodic-imu-filter-madgwick
+sudo apt install -y ros-melodic-rviz-imu-plugin
+sudo apt install -y ros-melodic-gmapping
+sudo apt install -y ros-melodic-map-server
